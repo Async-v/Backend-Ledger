1e7309aa08a284028d8dbf3db9bd51a29625008c
vikas <vikas301v@gmail.com>
2026-06-01 16:04:12 +0530

    initialize backend ledger with user authentication
    - created package.json with dependencies
    - added server.js to start the application
    - implemented express app in app.js with cookie-parser
    - established MongoDB connection in db.js
    - developed user registration and login controllers in auth.controller.js
    - defined user model with password hashing in user.model.js
    - set up authentication routes in auth.routes.js

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+
+const authRoutes = require('./routes/auth.routes')
+
+const app = express();
+
+//Middleware
+app.use(express.json());
+app.use(cookieParser())
+
+//Routes
+app.use('/api/auth', authRoutes)
+
+
+module.exports = app;
---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,77 @@
+const userModel = require('../models/user.model')
+const jwt = require('jsonwebtoken')
+
+/** 
+* - user register controller
+* - POST /api/auth/register
+*/
+
+async function userRegisterController(req, res) {
+
+    const { email, password, name } = req.body
+
+    const isExists = await userModel.findOne({ email: email })
+    if (isExists) {
+        return res.status(422).json({
+            status: "failed",
+            message: "User already exists with this email."
+        })
+    }
+
+    const user = await userModel.create({
+        email, password, name
+    })
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "3d" })
+
+    res.cookie("token", token)
+    res.status(201).json({
+        user: {
+            _id: user._id,
+            email: user.email,
+            name: user.name
+        },
+        token
+    })
+}
+
+/**
+ * - user login controller
+ * - POST /api/auth/login
+*/
+
+async function userLoginController(req, res) {
+    const { email, password } = req.body
+
+    const user = await userModel.findOne({ email: email }).select("+password")
+    if (!user) {
+        return res.status(401).json({
+            message: "Email or Password is Invaild"
+        })
+    }
+
+    const isVaildPassword = await user.comparePassword(password)
+    if (!isVaildPassword) {
+        return res.status(401).json({
+            message: "Email or Password is Invaild"
+        })
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "3d" })
+
+    res.cookie("token", token)
+    res.status(201).json({
+        user: {
+            _id: user._id,
+            email: user.email,
+            name: user.name
+        },
+        token
+    })
+}
+
+
+module.exports = {
+    userRegisterController,
+    userLoginController
+}
